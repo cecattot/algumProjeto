f5864c58a1beaace0d150bb7900752bf3173a5bc
cecattot <raulcctt@outlook.com>
2022-07-11 15:30:38 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Algum título</title>
+    <link rel="stylesheet" href="/algumProjeto/css/bootstrap.css">
+    <script type="text/javascript" src="/algumProjeto/js/bootstrap.js"></script>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <select class="form-select">
+                <option value="1">1</option>
+                <option value="1">2</option>
+                <option value="1">3</option>
+            </select class="form-select">
+        </div>
+    </div>
+</div>
+</body>
+</html>
